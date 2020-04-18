4d9cacc1ad7624302770ce327e87ccb6e8bc4767
dolphinlmg <dolphinlmg@naver.com>
2020-04-18 16:26:59 +0900

    Initial Commit

---
+++ Simple Note/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Note
+//
+//  Created by 이민규 on 2020/04/18.
+//  Copyright © 2020 이민규. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
